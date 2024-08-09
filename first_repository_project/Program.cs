fc480289f14f9fbeaaca57d83358bb81ef277b11
Regan Newman <renewman315@gmail.com>
2024-08-08 20:53:40 -0400

    fruits and veg

--- first_repository_project/Program.cs
+++ first_repository_project/Program.cs
@@ -1,9 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! lol");
-var list01 = new List<int>();
-for(int i = 0; i<10; i++)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyNamespace
+{
+    class Program
     {
-        if(i%2 == 0){
-            Console.WriteLine(i);
+        static void Main(string[] args)
+        {
+            string[] list_of_things = {"pineapple", "orange", "carrot", "banana", "broccoli", "lettuce", "mango"};
+            string[] list_of_fruit = {"apple", "grape", "orange", "pineapple", "tomato", "banana", "mango", "lime"};
+            string[] list_of_veg = {"carrot", "potato", "broccoli", "lettuce", "squash"};
+
+            List<string> fruits = new List<string>();
+            List<string> veg = new List<string>();
+
+            foreach(string item in list_of_things)
+            {
+                if(list_of_fruit.Contains(item.ToLower()))
+                {
+                    fruits.Add(item);
+                }
+                else if(list_of_veg.Contains(item.ToLower()))
+                {
+                    veg.Add(item);
+                }else
+                {
+                    Console.WriteLine("This item is not a fruit or veggie, please try again!");
+                }
+            }
+
+            Console.WriteLine("Here are the fruits: " );
+            foreach(string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+
+            Console.WriteLine("Here are the veg: " );
+            veg.ForEach(veggie => Console.WriteLine(veggie));
+            
+
         }
     }
+}
+
+
